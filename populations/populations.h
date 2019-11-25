5e97b6b15ceaa5ca8789d8ea8be48322dbf7ec11
andriesbron <andries@inourplace.nl>
2019-11-25 19:34:32 +0100

    Create populations.h

---
+++ populations/populations.h
@@ -0,0 +1,25 @@
+//
+//  populations.h
+//
+
+#ifndef populations_h
+#define populations_h
+
+#include <stdio.h>
+
+
+void POPU_population_float (float * af_X, uint16_t ai_Size, float * af_max, float * af_min,float *af_mean, float * af_stdev);
+
+
+float POPU_mean_float (float * af_X, uint16_t ai_Size);
+int8_t POPU_mean_8s (int8_t *apuc_X, uint16_t auc_XLength);
+uint8_t POPU_mean_8u (uint8_t *apuc_X, uint16_t auc_XLength);
+
+float POPU_max_float (float * af_X, uint16_t ai_Size);
+
+float POPU_deviation_float (float * af_X, uint16_t ai_Size);
+
+float POPU_median_float (float * af_X, uint16_t ai_Size);
+
+
+#endif /* populations_h */
