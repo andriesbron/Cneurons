2c63237db3badee112e402b851f61ca3d4f4d677
andriesbron <andries@inourplace.nl>
2019-11-28 19:55:41 +0100

    Update stringsandorgan.h

--- examples/stringsandorgan/stringsandorgan.h
+++ examples/stringsandorgan/stringsandorgan.h
@@ -1,1 +1,13 @@
-/**/
+//
+//  stringsandorgan.h
+//
+
+#ifndef stringsandorgan_h
+#define stringsandorgan_h
+
+#include <stdio.h>
+
+
+void EXAMPLE_stringsOrCathedral (void);
+
+#endif /* stringsandorgan_h */
