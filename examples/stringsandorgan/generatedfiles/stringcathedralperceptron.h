b23ca27e8cfb4f29960275201f39b78217245268
andriesbron <andries@inourplace.nl>
2019-11-28 20:01:17 +0100

    Create stringcathedralperceptron.h

---
+++ examples/stringsandorgan/generatedfiles/stringcathedralperceptron.h
@@ -0,0 +1,20 @@
+/*
+ * @file stringcathedralperceptron.h
+ * @author ccreator.py a sub library of Cneurons
+ */
+#ifndef STRINGCATHEDRALPERCEPTRON_H
+#define STRINGCATHEDRALPERCEPTRON_H
+
+
+/******************************************************************************
+ * Defines                                                                    *
+ ******************************************************************************/
+
+#define SC_PERCEPTRON_INPUT_SIZE (4u)
+#define SC_PERCEPTRON_INTERCEPT (-8.0)
+#define SC_PERCEPTRON_WEIGHT_0 (4.896)
+#define SC_PERCEPTRON_WEIGHT_1 (-4.123)
+#define SC_PERCEPTRON_WEIGHT_2 (7.119)
+#define SC_PERCEPTRON_WEIGHT_3 (3.540649)
+
+#endif /* STRINGCATHEDRALPERCEPTRON_H */
