d96e34001b4ba9b83b8396905a3b35885842e279
andriesbron <andries@inourplace.nl>
2019-11-28 19:54:14 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//
+
+#include <stdio.h>
+
+#include "examples/stringsandorgan/stringsandorgan.h"
+
+
+int main(int argc, const char * argv[])
+{
+
+    EXAMPLE_stringsOrCathedral();
+    
+    return 0;
+}
