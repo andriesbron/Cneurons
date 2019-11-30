52e72cf8ed1288cda8d724eebd56740d5b021f33
andriesbron <andries@inourplace.nl>
2019-11-30 14:33:41 +0100

    Create mlperceptron.h

---
+++ perceptron/mlperceptron.h
@@ -0,0 +1,32 @@
+//
+//  mlperceptron.h
+//  cNeurons
+//
+//  Created by andries bron on 11/30/19.
+//  Copyright © 2019 inourplace. All rights reserved.
+//
+
+#ifndef mlperceptron_h
+#define mlperceptron_h
+
+#include <stdio.h>
+
+/**
+ * @brief 16 bits unsigned perceptron.
+ */
+typedef struct PRCPT_mlPerceptron_float {
+    uint16_t inputSize;
+    float *inputWeights;
+    uint8_t hiddenSize;
+    float *hiddenWeights;
+    uint8_t outputSize;
+    float *outputWeights;
+
+    float intercept;
+} PRCPT_mlPerceptron_float_t;
+
+
+
+
+
+#endif /* mlperceptron_h */
